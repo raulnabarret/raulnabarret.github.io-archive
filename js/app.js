673e2beb97868c469b8c0b5c1e7c7c808b163699
raulnabarret <raulnabarret@gmail.com>
2016-07-17 19:55:10 -0500

    Agregar Google Maps

--- js/app.js
+++ js/app.js
@@ -2,13 +2,5 @@ $(document).ready(function() {
 	
 	$('.button-collapse').sideNav()
 
-	var map
-
-	function initMap() {
- 		map = new google.maps.Map($('#map'), {
-    		center: {lat: -34.397, lng: 150.644},
-    		zoom: 8
-  		})
-	}
-
+ 
 })
