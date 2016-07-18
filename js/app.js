1832a82c24747be998f57108c9fcfa5202febfcf
raulnabarret <raulnabarret@gmail.com>
2016-07-18 11:11:58 -0500

    Estilos en el header

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	
-	$('.button-collapse').sideNav()
+	$('.button-collapse').sideNav({		      
+		closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	})
 
 	$('.info-legal').click(function(e) {
 		e.preventDefault()
