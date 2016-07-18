c9fb59c8b6398d8b1afb92c1cf8093cf11b8c430
raulnabarret <raulnabarret@gmail.com>
2016-07-17 23:28:49 -0500

    Prevent Default de eventos en el footer

--- js/app.js
+++ js/app.js
@@ -2,5 +2,13 @@ $(document).ready(function() {
 	
 	$('.button-collapse').sideNav()
 
+	$('.info-legal').click(function(e) {
+		e.preventDefault()
+	})
+
+	$('.info-privacy').click(function(e) {
+		e.preventDefault()
+	})
+
  
 })
